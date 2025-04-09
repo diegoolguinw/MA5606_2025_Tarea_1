346d0c781001a8bc0e6ca65f1a4b0a65de6dc89b
diegoolguinw <74310240+diegoolguinw@users.noreply.github.com>
2025-04-08 22:32:20 -0400

    update

--- P2/tarea1_p2.py
+++ P2/tarea1_p2.py
@@ -1,15 +1,11 @@
 import numpy as np
+from scipy.interpolate import interp1d
 import warnings
-warnings.filterwarnings("ignore", category=UserWarning, module="sklearn")
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
 
 X = np.loadtxt('X.csv', delimiter=',', unpack=True)
 y = np.loadtxt('y.csv', delimiter=',', unpack=True)
 
-gp = GaussianProcessRegressor(kernel=RBF(length_scale_bounds=(1e-10,1e10)), alpha=0.1**2)
-gp.fit(X.reshape(-1, 1), y)
+interp = interp1d(X, y, kind='cubic', fill_value='extrapolate')
 
 def sample_f(x):
-    pred = gp.predict(x.reshape(-1, 1))
-    return pred*np.random.normal(1, 1, len(x))
+    return interp(x)*np.random.normal(1, 1, len(x))
